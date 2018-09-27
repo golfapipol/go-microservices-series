910ebd6f1473fb703c0a3731095a75d1e4740727
Apipol Sukgler <apipol@computerlogy.com>
2018-09-27 22:18:02 +0700

    [Edited]: use boltdb for storage and use it in handler

--- main.go
+++ main.go
@@ -9,5 +9,12 @@ var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	initializeBoltClient()
 	service.StartWebServer("6767")
+}
+
+func initializeBoltClient() {
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
 }
